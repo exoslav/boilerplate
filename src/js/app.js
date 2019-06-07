9962c42c7b387782aedb1e5799d9e1dcb3c6a57a
mnikl01 <martin.nikl@dixonscarphone.com>
2019-06-07 15:39:03 +0200

    feat: add fontAwesome support

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
 
+import initIcons from './initializeIcons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+initIcons();
+
 const wrapper = document.getElementById('app');
 
 const Component = styled.div`
   background: white
 `;
 
-ReactDOM.render(<Component>test</Component>, wrapper);
+const WelcomeComponent = () => (
+  <Component>
+    <p>Hello world!</p>
+    <FontAwesomeIcon icon="star" /> {/* solid icon */}
+    <FontAwesomeIcon icon={['far', 'star']} /> {/* regular icon */}
+  </Component>
+);
+
+ReactDOM.render(<WelcomeComponent />, wrapper);
---
+++ src/js/initializeIcons.js
@@ -0,0 +1,45 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faMotorcycle as fasMotorcycle,
+  faCarSide as fasCarSide,
+  faCarCrash as fasCarCrash,
+  faCarBattery as fasCarBattery,
+  faBus as fasBus,
+  faTruck as fasTruck,
+  faStar as fasStar,
+  faHome as fasHome,
+  faExclamationTriangle as fasExclamationTriangle,
+  faUser as fasUser,
+  faAlignJustify as fasAlignJustify,
+  faTh as fasTh,
+  faBriefcaseMedical as fasBriefcaseMedical,
+  faTrafficLight as fasTrafficLight,
+  faSign as fasSign,
+  faChalkboardTeacher as fasChalkboardTeacher,
+  faStickyNote as fasStickyNote,
+  faTimes as fasTimes,
+  faTimesCircle as fasTimesCircle,
+  faCheck as fasCheck,
+  faCheckCircle as fasCheckCircle,
+  faChevronDown as fasChevronDown,
+  faChevronUp as fasChevronUp,
+  faChevronRight as fasChevronRight,
+  faChevronLeft as fasChevronLeft
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  faStar as farStar,
+  faStickyNote as farStickyNote,
+  faSadTear as farSadTear
+} from '@fortawesome/free-regular-svg-icons';
+
+const initIcons = () => {
+  library.add(
+    fasMotorcycle, fasCarSide, fasCarCrash, fasCarBattery, fasBus, fasTruck, fasStar, farStar, fasHome,
+    fasExclamationTriangle, fasUser, fasAlignJustify, fasTh, fasBriefcaseMedical, fasTrafficLight, fasSign,
+    fasChalkboardTeacher, fasStickyNote, farStickyNote, fasTimesCircle, fasCheckCircle, farSadTear, fasChevronDown,
+    fasChevronRight, fasChevronLeft, fasChevronUp, fasTimes, fasCheck
+  );
+};
+
+export default initIcons;
